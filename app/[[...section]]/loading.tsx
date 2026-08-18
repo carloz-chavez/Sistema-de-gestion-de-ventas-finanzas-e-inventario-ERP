1b825daf26745c6d4552d5b47534f76ea97a2834
carloz-chavez <carlos.chavez.o@uni.pe>
2026-08-18 12:40:15 -0500

    feat: add loading and friendly error states

---
+++ app/[[...section]]/loading.tsx
@@ -0,0 +1,2 @@
+export default function Loading(){return <div className="loadingPage" role="status" aria-label="Cargando"><div className="loadingHead"/><div className="loadingStats">{[1,2,3,4].map(x=><span key={x}/>)}</div><div className="loadingPanel"/><p>Cargando información…</p></div>}
+
